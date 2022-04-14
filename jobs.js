78e30a5d8247284807dcc6f2446795ffc4141aad
Luke Harrison <lharriso@nyit.edu>
2022-04-13 23:02:59 -0400

    added some comments

--- jobs.js
+++ jobs.js
@@ -1,5 +1,6 @@
 const Alpaca = require("@alpacahq/alpaca-trade-api");
 const fs = require("fs");
+//set env variables in /etc/environment
 require("dotenv").config();
 const { Telegraf } = require("telegraf");
 const bot = new Telegraf(process.env.TELEGRAM_KEY);
