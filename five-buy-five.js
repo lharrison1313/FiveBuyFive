140e3ca7af14104b33a5fdf869defc5be339c5f4
Luke Harrison <lharriso@nyit.edu>
2022-04-23 09:21:24 -0400

    added movers commands

--- five-buy-five.js
+++ five-buy-five.js
@@ -1,6 +1,7 @@
 const { Telegraf } = require("telegraf");
 const Alpaca = require("@alpacahq/alpaca-trade-api");
 require("dotenv").config();
+const commands = ["positions", "pl", "winners", "losers", "movers"];
 const bot = new Telegraf(process.env.TELEGRAM_KEY);
 const alpaca = new Alpaca({
   keyId: process.env.PAPER_KEY_ID,
@@ -9,39 +10,87 @@ const alpaca = new Alpaca({
 });
 
 bot.launch();
-bot.command(["positions"], async (ctx) => {
-  switch (ctx.update.message.text) {
-    case "/positions":
-      await getPositions(ctx);
-      break;
-    default:
-      break;
-  }
-});
-
-async function getPositions(ctx) {
+bot.command(commands, async (ctx) => {
   try {
-    let chatID = ctx.update.message.chat.id;
-    let positions = await alpaca.getPositions();
-    let portfolio = await alpaca.getPortfolioHistory({
-      period: "all",
-    });
-    let totalPL = (portfolio.equity.at(-1) - portfolio.equity[0]) / portfolio.equity[0];
-    let formattedPositions = "Here Are Our Positions:\n";
-    formattedPositions = formattedPositions.concat("-----------------------------------------------\n");
-    formattedPositions = formattedPositions.concat(
-      `Total P/L: $${Number(portfolio.equity.at(-1)).toFixed(2)} (${Number(totalPL).toFixed(4)})\n`
-    );
-    formattedPositions = formattedPositions.concat("-----------------------------------------------\n");
-    positions.forEach((position) => {
-      formattedPositions = formattedPositions.concat(
-        `${position.symbol}: $${Number(position.market_value).toFixed(2)} (${Number(
-          position.unrealized_intraday_plpc
-        ).toFixed(2)})\n`
-      );
-    });
-    await bot.telegram.sendMessage(chatID, formattedPositions);
+    const chatID = ctx.update.message.chat.id;
+    switch (ctx.update.message.text) {
+      case "/positions":
+        let positions = await getPositions();
+        await bot.telegram.sendMessage(chatID, positions);
+        break;
+      case "/pl":
+        let pl = await getPL();
+        await bot.telegram.sendMessage(chatID, pl);
+        break;
+      case "/winners":
+        let winners = await getMovers("winners");
+        await bot.telegram.sendMessage(chatID, winners);
+        break;
+      case "/losers":
+        let losers = await getMovers("losers");
+        await bot.telegram.sendMessage(chatID, losers);
+        break;
+      case "/movers":
+        let movers = await getMovers("both");
+        await bot.telegram.sendMessage(chatID, movers);
+        break;
+      default:
+        break;
+    }
   } catch (error) {
     console.log(error);
   }
+});
+
+async function getPositions() {
+  let positions = await alpaca.getPositions();
+  let formattedPositions = "";
+  let pl = await getPL();
+  formattedPositions = formattedPositions.concat("-----------------------------------------------\n");
+  formattedPositions = formattedPositions.concat(pl);
+  formattedPositions = formattedPositions.concat("-----------------------------------------------\n");
+  formattedPositions = formattedPositions.concat(formatPositions(positions));
+  return formattedPositions;
+}
+
+async function getPL() {
+  let portfolio = await alpaca.getPortfolioHistory({
+    period: "all",
+  });
+  let totalPL = (portfolio.equity.at(-1) - portfolio.equity[0]) / portfolio.equity[0];
+  return `Total P/L: $${Number(portfolio.equity.at(-1)).toFixed(2)} (${Number(totalPL).toFixed(4)})\n`;
+}
+
+function formatPositions(positions) {
+  let formattedPositions = "";
+  positions.forEach((position) => {
+    formattedPositions = formattedPositions.concat(
+      `${position.symbol}: $${Number(position.market_value).toFixed(2)} (${Number(
+        position.unrealized_intraday_plpc
+      ).toFixed(2)})\n`
+    );
+  });
+  return formattedPositions;
+}
+
+async function getMovers(direction) {
+  let positions = await alpaca.getPositions();
+  let formattedPositions = "";
+  if (positions.length < 5) return "There are not enough assets to determine movers";
+  positions.sort((posA, posB) => posA.unrealized_intraday_plpc - posB.unrealized_intraday_plpc);
+  if (direction === "winners" || direction === "both") {
+    let winners = positions.slice(positions.length - 4, positions.length);
+    formattedPositions = formattedPositions.concat("-----------------------------------------------\n");
+    formattedPositions = formattedPositions.concat("Today's Winners\n");
+    formattedPositions = formattedPositions.concat("-----------------------------------------------\n");
+    formattedPositions = formattedPositions.concat(formatPositions(winners));
+  }
+  if (direction === "losers" || direction === "both") {
+    let losers = positions.slice(0, 5);
+    formattedPositions = formattedPositions.concat("-----------------------------------------------\n");
+    formattedPositions = formattedPositions.concat("Today's Losers\n");
+    formattedPositions = formattedPositions.concat("-----------------------------------------------\n");
+    formattedPositions = formattedPositions.concat(formatPositions(losers));
+  }
+  return formattedPositions;
 }
